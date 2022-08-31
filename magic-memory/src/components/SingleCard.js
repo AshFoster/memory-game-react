5e61eff2ca7243ca0d64c2b276a2f260ce4d55a5
AshFoster <ashfoster87@gmail.com>
2022-08-31 07:27:14 +0000

    Compare cards and add matched property if matched

--- magic-memory/src/components/SingleCard.js
+++ magic-memory/src/components/SingleCard.js
@@ -3,13 +3,23 @@ import React from 'react'
 // styles
 import './SingleCard.css'
 
-export default function SingleCard({ card }) {
-  return (
-    <div className='card'>
-        <div>
-            <img className='front' src={card.src} alt='card front'/>
-            <img className='back' src='/img/cover.png' alt='card back'/>
+export default function SingleCard({ card, handleChoice }) {
+
+    const handleClick = () => {
+        handleChoice(card)
+    }
+
+    return (
+        <div className='card'>
+            <div>
+                <img className='front' src={card.src} alt='card front'/>
+                <img 
+                    className='back' 
+                    src='/img/cover.png' 
+                    alt='card back' 
+                    onClick={handleClick}
+                />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
